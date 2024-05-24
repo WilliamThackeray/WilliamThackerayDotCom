12a37346e8af427e2960f57ad6a51962346f17a5
William Thackeray <will_thack@comcast.net>
2024-05-24 15:44:35 -0600

    inital commit - project setup

---
+++ frontend/wt.com/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar title="WilliamThackeray.com">
+      <v-tabs>
+        <v-tab>Home</v-tab>
+        <v-tab>About</v-tab>
+        <v-tab>Projects</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main> Main </v-main>
+
+    <v-footer>Footer</v-footer>
+  </v-app>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
